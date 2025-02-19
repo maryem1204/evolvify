7fca2ad2a6d5b711190922936dc5ed4551a816c8
Farahchebane22 <chebanefarah@gmail.com>
2025-02-19 09:50:41 +0100

    back

--- src/main/java/tn/esprit/Controllers/FrontController.java
+++ src/main/java/tn/esprit/Controllers/FrontController.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+package tn.esprit.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.scene.control.Button;
 
 public class FrontController {
+
+    @FXML
+    private Button Accocier_tr;
+
+    @FXML
+    private Button Creer_ab;
+
+    @FXML
+    private void openAbonnementWindow() {
+        openNewWindow("/add_abonnement.fxml", "Créer un abonnement");
+    }
+
+    @FXML
+    private void openTrajetWindow() {
+        openNewWindow("/add_trajet.fxml", "Associer un trajet");
+    }
+
+    private void openNewWindow(String fxmlPath, String title) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
